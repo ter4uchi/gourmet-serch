8437628063d67cc5d302fbbb020464aa1d38bc3e
Zetta-Terauchi <50479076+Zetta-Terauchi@users.noreply.github.com>
2021-04-06 15:27:57 +0900

    20210406

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,53 @@
 <template>
   <div>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <a href="http://webservice.recruit.co.jp/"><img src="http://webservice.recruit.co.jp/banner/hotpepper-s.gif" alt="ホットペッパー Webサービス" width="135" height="17" border="0" title="ホットペッパー Webサービス"></a>
+    <div v-for="restraunt in restraunts" :key="restraunt.id" class='card'>
+      <a :href="restraunt.urls.pc">{{restraunt.name}}
+        {{restraunt.genre.name}}
+        <img :src="restraunt.logo_image">
+      </a>
+    </div>
+    Powered by <a href="http://webservice.recruit.co.jp/">ホットペッパー Webサービス</a>
   </div>
 </template>
 
 <script>
-//import { Options } from '../node_modules/vue-class-component/dist/vue-class-component';
+import jQuery from 'jquery';
+
 export default{
   name:"App",
   data(){
     return{
+      restraunts:null,
 
     }
   },
-  created(){
-    console.log("location");
+  mounted(){
+    this.getData();
+  },
+  methods:{
+    getData(){
+      var self = this ;//eslint-disable-line
       const success = function(position){
-        console.log(position)
-        console.log(position.coords.latitude);
-        console.log(position.coords.longitude);
+      const lat = position.coords.latitude;
+      const lng = position.coords.longitude;
+      const URL ="http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=0336bf1d8a990721&lat="+lat+"&lng="+lng+"&range=2&order=4&format=jsonp&callback=callback";
+      jQuery.ajax({
+        type: 'GET',
+        url: URL,
+        dataType: 'jsonp',
+        jsonpCallback:'callback',
+        success: out => {
+          self.restraunts = out.results.shop;
+        }
+      });
       }
-    navigator.geolocation.getCurrentPosition(success)
+      navigator.geolocation.getCurrentPosition(success)
+    }
+  },
+  watch:{
+    restraunts:function() {
+      console.log(this.restraunts);
+    }
   }
 }
 </script>
@@ -33,6 +59,27 @@ export default{
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  display: flex;            /* Flexboxを指定 */
+  justify-content: center;  /* 水平方向の中央寄せ */
+}
+.card{
+  margin: 4%;
+  height: 150px;
+  border-radius: 50px;
+  background: #e0e0e0;
+  box-shadow:  10px 10px 30px #bebebe,
+              -10px -10px 30px #ffffff;
+}
+.card:active{
+  margin: 2%;
+  border-radius: 50px;
+  background: linear-gradient(145deg, #cacaca, #f0f0f0);
+  box-shadow:  10px 10px 30px #bebebe,
+              -10px -10px 30px #ffffff;
+}
+a{
+  display: block;
+  text-decoration: none;
+  color: #2c3e50;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
+import jQuery from 'jquery'
 
 createApp(App).mount('#app')
